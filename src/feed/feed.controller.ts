72b6e2144b5a22816a7063ae75b11376f2bc13ca
segun <segun408@gmail.com>
2024-03-10 20:44:57 +0900

    피드 수정, 삭제, 카테고리별 조회, 특정 피드 조회 구현

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Res, HttpStatus, Param } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { Feed } from './schemas/feed.schema';
 import { Response } from 'express';
@@ -8,6 +8,7 @@ export class FeedController {
 
   constructor(private readonly feedService: FeedService) {}
 
+  // 피드 생성
   @Post()
   async create(@Body() feed: Feed, @Res() res: Response): Promise<Feed> {
     const createdFeed = this.feedService.create(feed);
@@ -16,8 +17,39 @@ export class FeedController {
     return createdFeed;
   }
 
+  // 모든 피드 조회
   @Get()
   async findAll(): Promise<Feed[]> {
     return this.feedService.findAll();
+  }
+
+  // 특정 피드 조회
+  @Get('/:id')
+  async findOne(@Param('id') id: string): Promise<Feed> {
+    return this.feedService.findOne(id);
+  }
+
+  // 피드 수정
+  @Post('/:id')
+  async update(@Param('id') id: string, @Body() feed: Feed, @Res() res: Response): Promise<Feed> {
+    const updatedFeed = this.feedService.update(id, feed);
+    res.location('http://localhost:3000/'); // 주소는 나중에 수정
+    res.status(HttpStatus.FOUND).send();
+    return updatedFeed;
+  }
+
+  // 피드 삭제
+  @Post('/:id/delete')
+  async remove(@Param('id') id: string, @Res() res: Response): Promise<Feed> {
+    const removedFeed = this.feedService.remove(id);
+    res.location('http://localhost:3000/'); // 주소는 나중에 수정
+    res.status(HttpStatus.FOUND).send();
+    return removedFeed;
+  }
+
+  // 카테고리별 피드 조회
+  @Get('/category/:category')
+  async findByCategory(@Param('category') category: string): Promise<Feed[]> {
+    return this.feedService.findByCategory(category);
   }
 }
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -7,12 +7,44 @@ import { Feed, FeedDocument } from './schemas/feed.schema';
 export class FeedService {
   constructor(@InjectModel(Feed.name) private feedModel: Model<FeedDocument>) {}
 
+  // 피드 생성
   async create(feed: Feed): Promise<Feed> {
     const createdFeed = new this.feedModel(feed);
     return createdFeed.save();
   }
 
+  // 모든 피드 조회
   async findAll(): Promise<Feed[]> {
     return this.feedModel.find().exec();
+  }
+
+  // 특정 피드 조회
+  async findOne(id: string): Promise<Feed> {
+    return this.feedModel.findById(id);
+  }
+
+  // 피드 수정
+  async update(id: string, feed: Feed): Promise<Feed> {
+    return this.feedModel.findByIdAndUpdate(id, feed, { new: true });
+  }
+
+  // 피드 삭제
+  async remove(id: string): Promise<Feed> {
+    try{
+      let f = await this.feedModel.findById(id);
+      if(f.get('deletedAt') === undefined) {
+        f.set('deletedAt', Date.now());
+        return this.feedModel.findByIdAndUpdate(id, f, { new: true });
+      } else {
+        throw new Error('Feed already deleted');
+      }
+    } catch(e) {
+      return e;
+    }
+  }
+
+  // 카테고리별 피드 조회
+  async findByCategory(category: string): Promise<Feed[]> {
+    return this.feedModel.find({ category: category });
   }
 }
