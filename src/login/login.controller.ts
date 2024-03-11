ef586f451843e5811778d8748bd4955ce2312015
segun <segun408@gmail.com>
2024-03-11 13:29:16 +0900

    카카오 로그인 콘솔 구현

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -7,16 +7,16 @@ export class LoginController {
   @Get('kakao-login-page')
   @Header('Content-Type', 'text/html')
   async kakaoRedirect(@Res() res:Response): Promise<void> {
-    const Rest_api_key='676fceb5e807f864a04f35f7f7cd403c' //REST API KEY
-    const redirect_uri = 'http://localhost:3000/kakaoOauth' //Redirect URI
+    const Rest_api_key='b18a55ea373df95fa3136d22d2e3fa7f' //REST API KEY
+    const redirect_uri = 'http://localhost:8000/login/kakao' //Redirect URI
     
     const url = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
     res.redirect(url);
   }
-  // @Get('kakao')
-  // async getKakaoInfo(@Query() query: { code }) {
-  //   const apikey = '676fceb5e807f864a04f35f7f7cd403c';
-  //   const redirectUri = 'http://localhost:3000/kakaoOauth';
-  //   await LoginService.kakaoLogin(apikey, redirectUri, query.code);
-  // }
+  @Get('kakao')
+  async getKakaoInfo(@Query() query: { code }) {
+    const apikey = 'b18a55ea373df95fa3136d22d2e3fa7f';
+    const redirectUri = 'http://localhost:8000/login/kakao';
+    await LoginService.kakaoLogin(apikey, redirectUri, query.code);
+  }
 }
